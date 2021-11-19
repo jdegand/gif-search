d53df24845e05071e9bc30b422ccbe7396f12f99
jdegand <70610011+jdegand@users.noreply.github.com>
2021-11-19 16:08:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center">Gif Search</h1>
+
+<div align="center">
+   Tutorial from  <a href="https://makeschool.org/mediabook/oa/tutorials/makereddit/00-get-started/" target="_blank">makeschool.org</a>.
+</div>
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Improvements](#improvements)
+- [Useful Resources](#useful-resources)
+
+## Overview
+
+MakeSchool was a coding bootcamp that went under.  Most content was made free and I found this and decided to follow along.
+
+To run, git clone and add a Tenor API Key.
+
+![](./gif-search-desktop.png)
+
+
+### Built With
+
+- Express
+- Handlebars
+- Tenorjs
+
+## Features
+
+Simple search to an API and displaying the results.  
+
+## Improvements
+
+- Handlebars has problems with prototype pollution.  
+- Rework the fetching to tenor API.
+
+### Resources
+
+- npx express-generator --view=hbs ______ (project name) 
+
+- [YouTube](https://www.youtube.com/watch?v=EjhVucMRMbM) - Set up express
+- [Snyk](https://snyk.io/vuln/SNYK-JS-HBS-1566555) - hbs vulnerability
+- [Github](https://securitylab.github.com/advisories/GHSL-2021-020-pillarjs-hbs/) - security advisory hbs
+- [Stack Overflow](https://stackoverflow.com/questions/30051236/argument-passed-in-must-be-a-string-of-24-hex-characters-i-think-it-is) - warning hbs
+- [NPM](https://www.npmjs.com/package/express-handlebars) - express handlebars
+- [Stack Overflow](https://stackoverflow.com/questions/69959820/typeerror-exphbs-is-not-a-function)
+- [Stack Overflow](https://stackoverflow.com/questions/15463199/how-to-set-custom-favicon-in-express) - favicon in express 
---
+++ app.js
@@ -0,0 +1,60 @@
+// Require Libraries
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const Tenor = require("tenorjs").client({
+  "Key": "Replace with your own key", // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
+// App Setup
+const app = express();
+
+// Middleware
+app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// Routes
+
+/*
+app.get('/', (req, res) => {
+    // set the url of the gif
+    //const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    //res.render('hello-gif', { gifUrl })
+    console.log(req.query)
+    res.render('home')
+})
+*/
+
+app.get('/', (req, res) => {
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          // store the gifs we get back from the search
+          const gifs = response;
+          // pass the gifs as an object into the home page
+          res.render('home', { gifs })
+      }).catch(console.error);
+})
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+  })
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
